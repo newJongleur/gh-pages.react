adce58929ed96ff0b1579ccc2a88ee1f4d05e687
newJongleur <505755935@qq.com>
2022-11-24 14:56:37 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { HashRouter, Routes, Route } from "react-router-dom";// Hash模式
 import Home from './views/HomeView'
 import About from './views/AboutView'
-import About from './views/NotFoundView'
+import NotFound from './views/NotFoundView'
 
 export default () => {
   return (
@@ -15,7 +15,7 @@ export default () => {
           </Route> */}
           <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="*" element={<NotFoundView />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </HashRouter>
       {/* </BrowserRouter> */}
--- src/views/NotFoundView.jsx
+++ src/views/NotFoundView.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function NotFoundView() {
+export default () => {
     return (
         <>
             <label>404</label>
